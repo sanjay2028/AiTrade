66959c86fd122e9ce2d1f06800ecaa03c527d2eb
Sanjay Kumar <sanjay2028@gmail.com>
2020-10-14 08:56:03 +0530

    Optimzed the product thumb component

--- src/components/utils/product.js
+++ src/components/utils/product.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { PRODUCT_DEFAULT_EXCERPT } from '../../helpers/constants'
+import product_image_placeholder from '../../assets/images/fff.png';
 
+const ProductThumb = (props) => 
+        <Fragment>
+            <img className="card-img-top prd-thumb" {...props} />
+            <img className="card-img-top placeholder" src={product_image_placeholder} alt={props.title} />
+        </Fragment>
+    
 
 const ProductItem =  ({image, title, description, price, id:product_id}) => {
     title = title.length > 42? title.substr(0, 39) + "..." : title;
     description = description.length > 150? description.substr(0, 147) + " ..." : description;
     return( 
         <div className="card mb-3">
-            <div className="image-wrap">
-                    <img className="card-img-top prd-thumb" src={image} alt={title} />
-                    <img className="card-img-top placeholder" src="https://dummyimage.com/600x400/55595c/fff" alt={title} />                    
-                </div>                            
+            <div className="image-wrap"><ProductThumb src={image} alt={title} /></div>                            
             <div className="card-body">
                 <h4 className="card-title product-title"><Link to={`/product/${product_id}`}>{title}</Link></h4>
                 <p className="card-text product-card-description">{description}</p>
